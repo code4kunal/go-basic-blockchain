4f59c06e005bc1557b27c5cf9bc55ac2bd0e4a82
Kunal Saxena <kunal1991@hotmail.com>
2018-07-11 17:20:16 +0530

    Initial structure

---
+++ helper/helper.go
@@ -0,0 +1,69 @@
+package helper
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"time"
+	"go-blockchain/model"
+	"net/http"
+	"encoding/json"
+)
+var Blockchain []model.Block
+
+func RespondWithJSON(w http.ResponseWriter, r *http.Request, code int, payload interface{}) {
+	response, err := json.MarshalIndent(payload, "", "  ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("HTTP 500: Internal Server Error"))
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func CalculateHash(block model.Block) string {
+	record := string(block.Index) + block.Timestamp + string(block.BPM) + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
+}
+
+
+func GenerateBlock(oldBlock model.Block, BPM int) (model.Block, error) {
+
+	var newBlock model.Block
+
+	t := time.Now()
+
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = t.String()
+	newBlock.BPM = BPM
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = calculateHash(newBlock)
+
+	return newBlock, nil
+}
+
+func IsBlockValid(newBlock, oldBlock model.Block) bool {
+	if oldBlock.Index+1 != newBlock.Index {
+		return false
+	}
+
+	if oldBlock.Hash != newBlock.PrevHash {
+		return false
+	}
+
+	if CalculateHash(newBlock) != newBlock.Hash {
+		return false
+	}
+
+	return true
+}
+
+
+func ReplaceChain(newBlocks []model.Block) {
+	if len(newBlocks) > len(Blockchain) {
+		Blockchain = newBlocks
+	}
+}
